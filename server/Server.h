7c1de68f58d4d778af4a9473c5c8b46b1af6f3d8
Zhang Senhua <2796279605@qq.com>
2022-11-07 22:28:52 +0800

    增加命名空间，解决编译找不到头文件

--- server/Server.h
+++ server/Server.h
@@ -3,27 +3,30 @@
 #include <string>
 using namespace std;
 
-namespace yazi {
-namespace server {
-
-class Server
+namespace yazi
 {
-public:
-    Server();
-    ~Server();
+    namespace server
+    {
 
-    void listen(const string & ip, int port);
-    void start();
-    void set_threads(int threads);
-    void set_connects(int connects);
-    void set_wait_time(int wait_time);
+        class Server
+        {
+        public:
+            Server();
+            ~Server();
 
-private:
-    string m_ip;
-    int m_port;
-    int m_threads;
-    int m_connects;
-    int m_wait_time;
-};
+            void listen(const string &ip, int port);
+            void start();
+            void set_threads(int threads);
+            void set_connects(int connects);
+            void set_wait_time(int wait_time);
 
-}}
+        private:
+            string m_ip;
+            int m_port;
+            int m_threads;
+            int m_connects;
+            int m_wait_time;
+        };
+
+    }
+}
